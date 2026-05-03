fe54733734e791f2ae2bd15c1e21d3b976860b83
owen891 <owen891@users.noreply.github.com>
2026-05-03 04:54:59 +0000

    feat: 查看 Tmall Dashboard 代码
    Co-authored-by: traeagent <traeagent@users.noreply.github.com>

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -29,7 +29,7 @@ const routes = [
   {
     path: '/traffic',
     name: 'Traffic',
-    component: () => import('@/views/TrafficSimplified.vue')
+    component: () => import('@/views/Traffic.vue')
   },
   {
     path: '/traffic-analysis',
